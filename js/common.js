345ac8720984f869b320d6cdf56b31bdce167f69
Thiago Monteiro Barbosa <supermonteiro@gmail.com>
2017-12-19 12:10:24 -0400

    1.0.0
    Bugs fixed, dinamically colored icons, texts and labels, all colors
    added to json.

--- js/common.js
+++ js/common.js
@@ -1,6 +1,12 @@
 
 var activity_data = null;
+var data_answers = null;
 var correctIcons = 0;
+var primary_color = null,
+	border_color_1= null,
+	border_color_2= null,
+	accent_color_1 = null,
+	accent_color_2 = null;
 
 jQuery.fn.extend({
 	centerLabel: function() {
@@ -20,6 +26,65 @@ function initActivity() {
 	
 	TweenMax.set($('.circle-ico'), {opacity: 0});
 	
+	/* dinamically colored buttons */
+	primary_color = activity_data.primary_color;
+	border_color_1 = activity_data.border_color_1;
+	border_color_2 = activity_data.border_color_2;
+	accent_color_1 = activity_data.accent_color_1;	
+	accent_color_2 = activity_data.accent_color_2;
+	
+	$(".btn").css({
+		'background': primary_color			
+	});
+							
+	$(".btn").bind('mouseover', function() {		
+		$(this)[0].style.backgroundColor = accent_color_2;
+
+		$(".btn").bind('mouseout', function () {
+			$(this)[0].style.backgroundColor = primary_color;
+        });
+	});	
+	
+	css().bind(".title-1", {	
+		"color": accent_color_1			
+	});
+	
+	css().bind(".instructions", {	
+		"color": accent_color_1			
+	});
+	
+	css().bind(".circle-ico", {	  
+	  "background-color" : border_color_1
+	});
+	
+	css().bind(".circle-ico:hover", {	  
+	  "background-color" : border_color_2
+	});
+	
+	css().bind(".circle-ico .center", {	  
+	  "background-color" : primary_color
+	});
+	
+	css().bind(".circle-ico:hover .center ", {
+        "background-color" : accent_color_2
+    });
+	
+	css().bind(".circle-ico-answer:hover", {	  
+	  "border-color" : accent_color_2
+	});
+	
+	css().bind(".icons-labels", {	  
+	  "color" : primary_color
+	});
+	
+	css().bind(".right-mark", {	  
+	  "border-color" : primary_color
+	});
+	
+	css().bind(".wrong-mark", {	  
+	  "border-color" : primary_color
+	});
+	
 	/* randomly re-order the icons and labels*/
 	if (activity_data.randomize_options) {
 		activity_data.options.sort(function(a, b){return 0.5 - Math.random()});	
@@ -35,10 +100,31 @@ function initActivity() {
 			initCount = 0;
 	
 	$.each(activity_data.options, function(index, obj){
-		/* populate icons */		
-		var icons = $('<div class="btn_reveal circle-ico draggable" id="'+obj.icon_label+'">')
-			.data({index: "answer"+index, info: obj})
-			.html('<div class="center"><img src="'+(obj.icon)+'"><i class="grade" aria-hidden="true"></i></div></div>')		
+		/* populate icons */
+		var answerCode = null;
+		switch(obj.icon_label) {
+			case "Floors":
+				answerCode = "answer0"
+				break;
+			case "Trash":
+				answerCode = "answer1"
+				break;
+			case "Bathrooms":
+				answerCode = "answer2"
+				break;
+			case "Windows":
+				answerCode = "answer3"
+				break;
+			case "Tables":
+				answerCode = "answer4"
+				break;
+			default:
+				answerCode = 0
+		} 
+		
+		var icons = $('<div class="circle-ghost-ico"> </div><div class="btn_reveal circle-ico draggable" id="'+obj.icon_label+'">')
+			.data({index: answerCode, info: obj})
+			.html('<div class="center"><i class="grade" aria-hidden="true"></i></div>')		
 			.appendTo(iconsContainer);
 		
 		var icon = $('.circle-ico:eq('+index+')');				
@@ -56,7 +142,11 @@ function initActivity() {
 		/* populate labels */
 		var labels = $('<div class="icons-labels"><p id="ico-label-'+(index)+'">'+obj.icon_label+'</p> </div>')			
 			.appendTo(labelsContainer);
+									
+		TweenMax.fromTo(icon, .25, {opacity: 0}, {delay: (delay*index), opacity: 1, ease: Power2.easeOut});
+	});
 		
+	$.each(data_answers, function(index, obj){				
 		/* populate answers */		
 		var answers = $('<div class="answer">')
 			.data({answer: index, info: obj})
@@ -67,21 +157,16 @@ function initActivity() {
 		$('.droppable').droppable({
 			accept: '#icons div',      		
     		drop: handleDropEvent
-  		} );
-		
-		TweenMax.fromTo(icon, .25, {opacity: 0}, {delay: (delay*index), opacity: 1, ease: Power2.easeOut});
+  		});
 	});
-	
+		
 	$('.title-1').html(activity_data.title);
 	$('.question').html(activity_data.question);
 	$('.instructions').html(activity_data.instructions);
 	
-	if (!activity_data.allow_reset) {
-		$('.btn-reset').removeClass('btn_reveal');
-		$('.btn-reset').addClass('disabled');
-	} else {
-		$('.btn-reset').removeClass('disabled');
-		$('.btn-reset').addClass('btn_reveal');
+	if (activity_data.allow_reset) {
+		$('#resetBtn').removeClass('hidden');
+		$('#resetBtnDis').addClass('hidden');
 	}
 	
 }
@@ -91,17 +176,17 @@ function getData(call_data_file) {
 		url: call_data_file,
 		success: function(data) {
 			activity_data = data;
+			data_answers = activity_data.options.slice().sort();
 			initActivity();
 		}})
 		.fail(function() { console.log( '*** load error: call_data.json ***'); });
 }
 
-function initListeners() {
-	
+function initListeners() {			
 	$(window).on('load resize orientationchange', function (e) {
 		$.each($('.icons-labels'), function(index, obj){
 			$(this).centerLabel();
-		});				
+		});					
 	});
 }
 
@@ -125,8 +210,7 @@ function handleDropEvent( event, ui ) {
 
 function gradeMarks() {
 	$.each($('.correct'), function(index, obj) {		
-		$('.correct i').addClass('fa fa-check right-mark');
-		//$('.correct').removeClass('correct');
+		$('.correct i').addClass('fa fa-check right-mark');		
 	});
 	$.each($('.wrong'), function(index, obj) {
 		$('.wrong i').addClass('fa fa-times wrong-mark');
@@ -140,31 +224,7 @@ function gradeMarks() {
 	$('#submitBtn').addClass('hidden');	
 }
 
-function retry(event, ui) {
-	/*
-	$.each($('.ui-draggable-disabled'), function(index, obj) {				
-		$('.ui-draggable-disabled').removeClass('ui-draggable-disabled');
-		ui.draggable.draggable( 'option', 'revert', 'invalid' );		
-	});
-	
-	$.each($('.ui-droppable-disabled'), function(index, obj) {				
-		$('.ui-droppable-disabled').removeClass('ui-droppable-disabled');
-	});
-	
-	$.each($('.correct'), function(index, obj) {		
-		$('.correct i').removeClass('fa fa-check right-mark');
-		$('.correct').removeClass('correct');
-	});
-	
-	$.each($('.wrong'), function(index, obj) {		
-		$('.wrong i').removeClass('fa fa-times wrong-mark');
-		$('.wrong').removeClass('wrong');
-	});
-	
-	correctIcons = 0;
-	$('#submitBtn').removeClass('hidden');	
-	$('#retryBtn').addClass('hidden');
-	*/
+function retry(event, ui) {	
 	location.reload();
 }
 
